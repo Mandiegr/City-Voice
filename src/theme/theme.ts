d1f08f15020b84a597ce363984c4a3f2b3a1becc
Mandiegr <nannda076@gmail.com>
2023-12-02 01:23:01 -0300

    feat: theme file

---
+++ src/theme/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  Colors: {
+    white: '#fff',
+    dark: '#000',
+    primary: '#0d47a1',
+    secondary: '#2196F3',
+    tertiary: '#64b5f6',
+    red: '#c1121f',
+    green: '#27a300',
+    orange: '#fe7f2d',
+  },
+
+  fontSizes: {
+    small: '14px',
+    medium: '16px',
+    large: '20px',
+  },
+};
+
+export default theme;
