16b16ec690230d3fcc3c80c4f5ea1b76a48a0fbc
Mandiegr <nannda076@gmail.com>
2023-12-01 22:57:42 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {HomeScreen} from './src/Screens/Home';
+
+const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/Screens/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, SafeAreaView, Text, ScrollView} from 'react-native';
+
+export const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Pagina inicial</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
